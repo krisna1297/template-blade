f681090ea9318c2e47191a706fcf83298db0e363
Krisna <66776107+krisna1297@users.noreply.github.com>
2020-07-01 21:33:47 +0700

    added adminlte

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/register', "AuthController@register")->name('register');
 Route::post('/welcome', "HomeController@welcome")->name('welcome');
+
+Route::get('/master', function () {
+    return view('adminlte.master');
+});
+
+Route::get('/items', function () {
+    return view('items.index');
+});
+
+Route::get('/items/create', function () {
+    return view('items.create');
+});
+*/
+
+Route::get('/', function () {
+    return view('table.index');
+});
+
+Route::get('/data-tables', function () {
+    return view('table.datatable');
+});
